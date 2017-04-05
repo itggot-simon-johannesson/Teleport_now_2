a23e972a27d2d42cee1ea4ce7f2038f499003e23
Simon <simon.johannesson@itggot.se>
2017-04-05 10:20:46 +0200

    ja

---
+++ site/controller.rb
@@ -0,0 +1,18 @@
+# require 'date'
+layout('layout.html.erb')
+
+# before do
+#     @root_pages = {
+#         'Home' => 'banan.html',
+#         'About Us' => 'index.html'
+#     }
+# end
+
+# functions inside the "helpers" block are available in the erb files.
+# helpers do
+#
+#     def nicely_formatted_date(date)
+#         return date.strftime("%Y-%m-%d %H:%M")
+#     end
+#
+# end
