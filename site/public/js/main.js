fd3fc9f111b55798d0dea699da4824e62e3a3165
Simon <simon.johannesson@itggot.se>
2017-04-19 12:36:32 +0200

    sista

--- site/public/js/main.js
+++ site/public/js/main.js
@@ -1,15 +1,15 @@
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML =
-    h + ":" + m;
-    var t = setTimeout(startTime, 500);
+
+function js_toggle_in_switch() {
+    element = document.querySelector(".in_switch")
+    element.classList.toggle("in_switch_toggle")
 }
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};
-    return i;
+
+function js_toggle_switch() {
+    element = document.querySelector(".switch")
+    element.classList.toggle("switch_toggle")
+}
+
+function js_toggle_favourites() {
+    element = document.querySelector(".favourites")
+    element.classList.toggle("favourites_toggle")
 }
